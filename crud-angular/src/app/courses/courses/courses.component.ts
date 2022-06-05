affbd3b4f718def1a578ca24d3620c61b8be04f3
lucasboot <varela.alves@outlook.com>
2022-06-05 12:01:25 -0300

    aula 20+ to go

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -12,9 +13,12 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(private coursesService: CoursesService, public dialog: MatDialog) {
+  constructor(private coursesService: CoursesService,
+     public dialog: MatDialog,
+     private  router: Router,
+     private route: ActivatedRoute) {
     //this.courses = [];
     this.courses$ = this.coursesService.listAll().pipe(
       catchError(error => {
@@ -27,6 +31,11 @@ export class CoursesComponent implements OnInit {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg
     });
+  }
+  onAdd(){
+    console.log('onAdd');
+    this.router.navigate(['new'], {relativeTo: this.route});
+
   }
   ngOnInit(): void {
   }
